24dfe771de935d441c0cc250f5be069d9bb50548
Tunbola Ogunwande <tunbolawande@yahoo.com>
2019-03-11 18:35:24 +0100

    CGP-1256: Add classes for processing trigger actions for Engagement scoring.

--- sql/create_engagement_action.sql
+++ sql/create_engagement_action.sql
@@ -1,5 +1,5 @@
 INSERT INTO civirule_action
 (name, label, class_name, is_active)
 VALUES
-("decrease_engagement_score", "Increase engagement score by X points", "CRM_CiviEngagementScoring_Rules_Actions_ContactEngagementScore", 1),
-("increase_engagement_score", "Decrease engagement score by X points", "CRM_CiviEngagementScoring_Rules_Actions_ContactEngagementScore", 1);
+("decrease_engagement_score", "Increase engagement score by X points", "CRM_CiviEngagementScoring_Rules_Actions_IncreaseEngagementScore", 1),
+("increase_engagement_score", "Decrease engagement score by X points", "CRM_CiviEngagementScoring_Rules_Actions_DecreaseEngagementScore", 1);
